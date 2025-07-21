02bd90972a81222e1ffa505c307e9243c91b70df
victorz41 <victorz41343@gmail.com>
2025-07-21 05:34:24 -0400

    work done

--- Assets/Scripts/PlayerDie.cs
+++ Assets/Scripts/PlayerDie.cs
@@ -1,6 +1,19 @@
+
 using UnityEngine;
 
-public class PlayerDie : MonoBehaviour
-{
 
-}
+public class PlayerDie: MonoBehaviour
+    {
+    public GameObject endPanel; 
+    private string Enemy = "Enemy";
+    
+   
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    if(collision.gameObject.tag == Enemy)
+        {
+        endPanel.SetActive(true);
+        gameObject.SetActive(false);
+        }
+    }
+    }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,29 @@
+
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement: MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
 
+    private float _xSpeed;
+    private float _ySpeed;
+
+    public float speed = 3;
+
+    private string InputX = "Horizontal";
+    private string InputY = "Vertical";
+    
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+    _xSpeed = Input.GetAxis(InputX); 
+    _ySpeed =Input.GetAxis(InputY);
+
+    _rigidbody2D. velocity = new Vector2(_xSpeed, _ySpeed) * speed;
+    }
 }
